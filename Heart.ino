0b6459916e0fa07a1dce84fa7d8540706468adcf
zankorobos13 <aruseniys13@yandex.ru>
2026-03-15 00:33:16 +0300

    Удалось выполнить первое подключение к wifi

--- Heart.ino
+++ Heart.ino
@@ -1,12 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(16, OUTPUT);
-}
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "config.h"
 
+WiFiData wifi_data;
+const char* SSID = wifi_data.SSID;
+const char* PASSWORD = wifi_data.PASSWORD;
+const char* SERVER_URL = wifi_data.SERVER_URL;
+
+void setup() {
+  Serial.begin(74880);
+  
+  WiFi.begin(SSID, PASSWORD);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi подключен");
+  Serial.print("IP адрес: ");
+  Serial.println(WiFi.localIP());
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  digitalWrite(16, LOW);
-  delay(1000);
-  digitalWrite(16, HIGH);
 }
