2a04e3a7e9f424a1807630bfc5723184dabc3bf4
zankorobos13 <aruseniys13@yandex.ru>
2026-03-21 03:10:42 +0300

    Добавлена функция прочтения сообщения и таким образом изменения его статуса

--- index/index.php
+++ index/index.php
@@ -5,7 +5,8 @@
     <h1>Form</h1>
 
     <form action = "requests_processing.php" method = "post">
-        <input type="password" name = "password" placeholder = "password"/><br>
+        <input type ="hidden" name = "mode" value = "post_message"/>
+        <input type= "password" name = "password" placeholder = "password"/><br>
         <textarea name = "message" placeholder = "message"></textarea><br>
         
         <input type="submit" value = "Enter" class = "btn btn-success"/>
--- index/requests_processing.php
+++ index/requests_processing.php
@@ -3,25 +3,54 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-define('MSG_FILE_NAME', 'message.txt');
+define('MSG_FILE_NAME', 'message.json');
+
+function sendPostToSelf($data) {
+    $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+    $response = curl_exec($ch);
+    
+    return $response;
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $password = $_POST['password'];
-    $message = $_POST['message'];
-    
-    $password = hash('sha256', $password);
-    $message = htmlentities($message);
+    $mode = $_POST['mode'];
 
-    if (true){
-        $message_data = [
-            'is_readed' => false,
-            'message' => $message
-        ];
-        $fd = fopen(MSG_FILE_NAME, 'w');
-        fwrite($fd, json_encode($message_data));
-        fclose($fd);
+    if ($mode == 'post_message'){
+        $password = $_POST['password'];
+        $message = $_POST['message'];
+        
+        $password = hash('sha256', $password);
+        // $message = htmlentities($message);
+
+        if (true){
+            $message_data = [
+                'is_readed' => false,
+                'message' => $message
+            ];
+
+            $json_string = json_encode($message_data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents(MSG_FILE_NAME, $json_string);
+        }
+    } elseif ($mode == 'post_message_status') {
+        $password = $_POST['password'];
+        $password = hash('sha256', $password);
+
+        $message_data = json_decode(file_get_contents(MSG_FILE_NAME), true);
+
+        if (true){
+            $message_data['is_readed'] = true;
+
+            $json_string = json_encode($message_data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents(MSG_FILE_NAME, $json_string);
+        }
     }
-
+    
     header('Location: index.php');
     exit;
 }
@@ -38,6 +67,13 @@ if (count($_GET) != 0){
             'message' => $message
         ]
     ];
+
+    $post_data = [
+        'mode' => 'post_message_status',
+        'password' => ''
+    ];
+
+    sendPostToSelf($post_data);
 
     echo json_encode($response);
     exit;
